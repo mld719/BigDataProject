4e6009728b70d36203b69ba6012e3f9f01166c8d
mld719 <megan.delaunay@richmond.edu>
2014-04-14 15:16:14 -0400

    SongMap now has topArtist method

--- SongMap.java
+++ SongMap.java
@@ -1,10 +1,12 @@
+// Authors: Cody Frenzel & Megan DeLaunay
+
 import java.util.*;
 import java.io.*;
 
 public class SongMap {
 	private HashMap<String, SongRecord> songCollection;
 	private String folderPath = "temp/file/path/";
-	
+
 	public SongMap(){
 		songCollection = new HashMap<String, SongRecord>();
 		read(folderPath);
@@ -22,19 +24,14 @@ public class SongMap {
         		}
 
         		Scanner scanner = null;
-        
-        		//try to read the file and catch the fileNotFoundException if you cannot
-        		//read it
         		try {
             		scanner = new Scanner(file);
-            
         		} catch (FileNotFoundException e) {
             		System.err.println("The file you requested does not exist.");
             		System.exit(1);
         		}
         		
         		String filename = file.getName();
-        		
         		//put lyrics in a string
         		String lyrics = "";
         		//while there is another line to read in the file 
@@ -43,12 +40,95 @@ public class SongMap {
             		lyrics = lyrics + line + "\n";
         		}	
 
-        		SongRecord record = new SongRecord(file);
+        		SongRecord record = new SongRecord(file, lyrics);
         		String artistAndSong = record.getArtist() + " - " + record.getSongName();
             	songCollection.put(artistAndSong, record);
+            	scanner.close();
 			}
 		}
 	}
+
+	public void songsByArtist(String artistName){
+		Set<String> songSet = songCollection.keySet();
+		//Iterator<String> iter = songSet.iterator();
+		int numSongs = 0;
+		for(String next: songSet)
+		{
+			//String next = iter.next();
+			if(next.contains(artistName)){
+				numSongs++;
+				SongRecord record = songCollection.get(next);
+
+				System.out.print("#" + record.getRank() + ". " + record.getSongName() +
+				 	" by " + record.getArtist() + ": " + "\n" + record.getLyrics());
+				System.out.println("----------------------------------------------------");
+
+			}
+		}
+
+		if(numSongs == 0){
+			System.out.println("There were no results that matched: " + artistName);
+		}
+	}
+
+	public String topArtist()
+	{
+	    Collection<SongRecord> recordsTMP = songCollection.values();
+	    Object[] recordsOBJ = recordsTMP.toArray();
+	    SongRecord[] records = (SongRecord[]) recordsOBJ;
+	    
+	    
+	    HashMap<String, Integer> artistCounts = new HashMap<String, Integer>();
+	    
+	    for(int i = 0; i < records.length; i++)
+	    {
+	        //for each song, find artist.
+	        
+	        SongRecord r = records[i];
+	        String artistName = r.getArtist();
+	        
+	        if(artistCounts.containsKey(artistName))
+	        {
+	            int value = artistCounts.get(artistName);
+	            artistCounts.remove(artistName);
+	            
+	            artistCounts.put(artistName, value++);
+	        }
+	        else
+	        {
+	            artistCounts.put(artistName, 1);	            
+	        }
+	        //if artist is novel, add to hashmap as artist, 1
+	        //if artist is NOT novel, increase its value by 1
+	    }
+	    
+	    //now, get values and find highest
+	    Object[] artistsOBJ = (artistCounts.keySet()).toArray();
+	    String[] artists = (String[]) artistsOBJ;
+	    
+	    Object[] countsOBJ = (artistCounts.values()).toArray();
+	    Integer[] counts = (Integer[]) countsOBJ;
+	    
+	    Integer highest = counts[0];
+	    
+	    for(int i = 1; i < counts.length; i++)
+	    {
+	        Integer two = counts[i];
+	        
+	        if(two.compareTo(highest) >= 0)
+	        {
+	            highest = two;
+	        }
+	        else
+	        {
+	            continue;
+	        }
+	    }
+	    
+	    
+	    return artists[highest];
+	}
+
 
 
 }
--- SongRecord.java
+++ SongRecord.java
@@ -1,44 +1,47 @@
-// Authors: Cody Frenzel & Megan DeLaunay
-
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class SongRecord {
-    private String artist;
-    private String lyrics;
-    private int rank;
+	private String artist;
+	private String songName;
+	private String lyrics;
+	private int rank;
 
-    public SongRecord(){
-        artist = null;
-        lyrics = null;
-        rank = 0;
-    }
+	public SongRecord(){
+		artist = null;
+		songName = null;
+		lyrics = null;
+		rank = 0;
+	}
 
-    public SongRecord(File file){
-        //get key which is artist and song, get the rank
+	public SongRecord(File file, String _lyrics){
+		//get key which is artist and song, get the rank
         String[] filename = file.getName().split(" -- ");
-        String strRank = filename[0];
+        rank = Integer.parseInt(filename[0]);
         // each file ends in "... song .txt" to end at the g is 6 spaces
         int fileEnding = 6; 
         String artistAndSong = filename[1].substring(1, filename[1].length() - fileEnding);
         String[] tmp = artistAndSong.split(" - ");
-        arti    st = tmp[0];
-        lyrics = tmp[1];
-    }
+        artist = tmp[0];
+        songName = tmp[1];
+        lyrics = _lyrics;
+	}
 
-    public string getArtist()
-    {
-        return artist;
-    }
-    
-    public string getSongName()
-    {
-        return lyrics;
-    }
-    
-    public string getLyrics()
-    {
-        return lyrics();
-    }
-    
+	public String getArtist(){
+		return artist;
+	}
+
+	public String getSongName(){
+		return songName;
+	}
+
+	public String getLyrics(){
+		return lyrics;
+	}
+
+	public int getRank(){
+		return rank;
+	}
+
+
 }
